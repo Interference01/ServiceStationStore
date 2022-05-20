04ddb94faaec6e492a8397665d9c3e27ea80a1fb
Interference01 <89568577+Interference01@users.noreply.github.com>
2022-05-20 19:15:56 +0300

    Закончена вкладка Товары. Переходим к поправке БД

--- ServiceStationStore/Models/Product.cs
+++ ServiceStationStore/Models/Product.cs
@@ -11,6 +11,7 @@ namespace ServiceStationStore.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+        public string Brand { get; set; }
         public int Quantity { get; set; }
         
     }
